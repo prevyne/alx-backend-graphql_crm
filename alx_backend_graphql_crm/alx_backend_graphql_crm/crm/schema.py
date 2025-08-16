3e0e41f489535302784940a99b3c3cb345437e5d
prevyne <prevyneoketch6@gmail.com>
2025-08-16 03:23:56 +0300

    set up graphql endpoints

--- alx_backend_graphql_crm/alx_backend_graphql_crm/crm/schema.py
+++
@@ -1,7 +0,0 @@
-import graphene
-
-class Query(graphene.ObjectType):
-    """
-    Defines the root query fields for the CRM API
-    """
-    hello = graphene.String(default_value="Hello")
--- alx_backend_graphql_crm/alx_backend_graphql_crm/schema.py
+++
@@ -1,8 +0,0 @@
-import graphene
-import crm.schema
-
-#Inherit from the apps's Query class
-class Query(crm.schema.Query, graphene.ObjectType):
-    pass
-
-schema = graphene.Schema(query=Query)
